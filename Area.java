9f16ed50234e80537da6a8462f2dc7e4b56f55ee
SimranKamboj320 <simrankamboj320@gmail.com>
2024-07-16 00:30:48 +0530

    Area of a Square
    Area of a Square taking as input

---
+++ Area.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Area{
+public static void main(String args[]){
+    Scanner sc = new Scanner(System.in);
+    int side = sc.nextInt();
+   
+    int Area = side*side;
+    System.out.println(Area);
+}
+}
