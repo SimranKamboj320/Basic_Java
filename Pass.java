1a1f9d81410cb66e8834f8e4eb18c31e8ec07c1e
SimranKamboj320 <simrankamboj320@gmail.com>
2024-07-16 15:47:33 +0530

    Pass or Fail
    Pass or Fail

---
+++ Pass.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Pass{
+    public static void main(String args[]){
+        int marks = 12;
+
+        String report = (marks>=33? "Pass": "Fail");
+        System.out.println(report);
+    }
+}
