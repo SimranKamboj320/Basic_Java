0c604b73b6c15fa2579a973ce25347e513ada427
SimranKamboj320 <simrankamboj320@gmail.com>
2024-07-16 15:46:15 +0530

    Largest of 3 numbers
    Largest of 3 numbers

---
+++ LargestThree.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class LargestThree{
+    public static void main(String args[]){
+        int a = 1;
+        int b = 3;
+        int c = 6;
+
+        if(a>b && a>c){
+            System.out.println(a);
+        }
+        else if(b>c){
+            System.out.println(b);
+        }
+        else{
+            System.out.println(c);
+        }
+    }
+}
