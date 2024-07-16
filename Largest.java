9cf10ed2b1bda3a40937cba213eb7d29f9d67612
SimranKamboj320 <simrankamboj320@gmail.com>
2024-07-16 15:46:01 +0530

    Largest of 2 numbers
    Largest of 2 numbers

---
+++ Largest.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Largest{
+    public static void main(String args[]){
+        int a = 45;
+        int b = 32;
+
+        if(a>b){
+            System.out.println(a);
+        }
+        else{
+            System.out.println(b);
+        }
+    }
+}
