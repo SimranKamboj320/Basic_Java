cf68ef04c0e1009f1692680edad10e661b02e12e
SimranKamboj320 <simrankamboj320@gmail.com>
2024-07-16 15:46:50 +0530

    Verify even or odd number
    Verify even or odd number

---
+++ OddEven.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class OddEven{
+    public static void main(String args[]){
+        int a = 45;
+
+        if(a%2==0){
+            System.out.println("a is even");
+        }
+        else{
+            System.out.println("a is odd");
+        }
+    }
+}
